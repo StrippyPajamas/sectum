268302c7e48df6a06f574ca1812fdd99a4936d14
strippyPajamas <johnny_pan_sg@outlook.com>
2024-12-31 13:24:12 +0800

    Initial commit

---
+++ src/client/java/strippypajamas/sectum/SectumClient.java
@@ -0,0 +1,10 @@
+package strippypajamas.sectum;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class SectumClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
---
+++ src/client/java/strippypajamas/sectum/SectumDataGenerator.java
@@ -0,0 +1,11 @@
+package strippypajamas.sectum;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class SectumDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
